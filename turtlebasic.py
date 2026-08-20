bc98a78e3dcbf1c4ad658e300bded08e2dd1b039
zahin <ishmamzahin404@gmail.com>
2026-08-20 11:12:20 +0600

    changed

--- turtlebasic.py
+++ turtlebasic.py
@@ -1,18 +1,19 @@
 import turtle
-import math
 
-def to_radian(degree):
-    return (degree * math.pi) / 180
+# Clipping window boundaries
+x_min, x_max = -100, 300
+y_min, y_max = -50, 200
 
-def draw_polygon(t, poly, color):
-    t.pensize(2)
+# Region codes
+LEFT, RIGHT, BOTTOM, TOP = 1, 2, 4, 8
+
+def draw_line(t, x1, y1, x2, y2, color):
     t.pencolor(color)
+    t.pensize(2)
     t.penup()
-    t.goto(poly[0][0], poly[0][1])
+    t.goto(x1, y1)
     t.pendown()
-    for i in range(len(poly)):
-        next_vertex = poly[(i + 1) % len(poly)]
-        t.goto(next_vertex[0], next_vertex[1])
+    t.goto(x2, y2)
     t.penup()
 
 def draw_axes(t, width, height):
@@ -30,57 +31,96 @@ def draw_axes(t, width, height):
     t.pendown()
     t.goto(0, height / 2)
     t.write("Y", align="center", font=("Arial", 12, "normal"))
-    
     t.penup()
 
-def mark_pivot_point(t, pivot_point, color):
-    t.penup()
-    t.goto(pivot_point[0], pivot_point[1])
-    t.dot(10, color)
+def region_code(x, y):
+    code = 0
+    if x < x_min:
+        code |= LEFT
+    elif x > x_max:
+        code |= RIGHT
+    if y < y_min:
+        code |= BOTTOM
+    elif y > y_max:
+        code |= TOP
+    return code
 
+def cohen_sutherland(x1, y1, x2, y2):
+    accept = False
+
+    while True:
+        code1 = region_code(x1, y1)
+        code2 = region_code(x2, y2)
+        if (code1 | code2) == 0:    # completely inside
+            accept = True
+            break
+        elif (code1 & code2) != 0:  # completely outside
+            break
+        else:
+            code = code1 if code1 != 0 else code2
+            if code & TOP:
+                y = y_max
+                x = x1 + (x2 - x1) * (y - y1) / (y2 - y1)
+            elif code & BOTTOM:
+                y = y_min
+                x = x1 + (x2 - x1) * (y - y1) / (y2 - y1)
+            elif code & LEFT:
+                x = x_min
+                y = y1 + (y2 - y1) * (x - x1) / (x2 - x1)
+            elif code & RIGHT:
+                x = x_max
+                y = y1 + (y2 - y1) * (x - x1) / (x2 - x1)
+            
+            if code == code1:
+                x1, y1 = x, y
+            else:
+                x2, y2 = x, y
+    if accept:
+        return (x1, y1, x2, y2)
+    else:
+        return None
+
+WIDTH, HEIGHT = 800, 600
 screen = turtle.Screen()
-screen.title("2D Polygon Rotation")
-screen.setup(width=800, height=600)
+screen.title("Cohen-Sutherland Line Clipping")
+screen.setup(width=WIDTH, height=HEIGHT)
 screen.bgcolor("white")
 
 t = turtle.Turtle()
 t.hideturtle()
-t.hideturtle()
-t.speed(0.6)
+t.speed(0)
 t.pensize(2)
 
-original_coordinates = [
-    (150, 150),
-    (250, 200),
-    (200, 250)
-]
-angle = 45
-rx, ry = (50, 50)
 
-draw_axes(t, 800, 600)
-mark_pivot_point(t, (rx, ry), "black")
 
-rotated_coordinates = []
+lines = []
 
-for point in original_coordinates:
-    x, y = point
+with open("lines.txt", "r") as file:
+    next(file)  # Skip the header line
 
-    x_shifted = x - rx
-    y_shifted = y - ry
+    for line in file:
+        x1, y1, x2, y2 = map(float, line.split())
+        lines.append((x1, y1, x2, y2))
 
-    rad_angle = to_radian(angle)
-    cos_angle = math.cos(rad_angle)
-    sin_angle = math.sin(rad_angle)
-    
-    x_rotated = x_shifted * cos_angle - y_shifted * sin_angle
-    y_rotated = x_shifted * sin_angle + y_shifted * cos_angle
 
-    final_x = x_rotated + rx
-    final_y = y_rotated + ry
-    
-    rotated_coordinates.append((final_x, final_y))
+draw_axes(t, WIDTH, HEIGHT)
 
-draw_polygon(t, original_coordinates, "orange")
-draw_polygon(t, rotated_coordinates, "black")
+# Draw the clipping rectangle
+t.pencolor("black")
+t.penup()
+t.goto(x_min, y_min)
+t.pendown()
+t.goto(x_max, y_min)
+t.goto(x_max, y_max)
+t.goto(x_min, y_max)
+t.goto(x_min, y_min)
+t.penup()
 
-screen.mainloop()
+
+for x1, y1, x2, y2 in lines:
+    draw_line(t, x1, y1, x2, y2, 'lightgray')
+    clipped_line = cohen_sutherland(x1, y1, x2, y2)
+    if clipped_line:
+        draw_line(t, clipped_line[0], clipped_line[1], clipped_line[2], clipped_line[3], 'black')
+
+turtle.done()
